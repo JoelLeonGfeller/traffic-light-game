44c223453bd02cdff12e54ce79281f3c813699da
Joel Leon Gfeller <joelleon.gfeller@icloud.com>
2023-07-31 09:04:13 +0100

    add function to main buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,31 @@
-console.log("connected!")
+const gameArea = document.querySelector(".game-area");
+const buttonsArea = document.querySelector(".main-buttons");
+
+const startGameBtn = document.getElementById("start-game");
+const rulesBtn = document.getElementById("read-rules");
+
+function randomId() {
+    return Math.ceil(Math.random() * 3);
+}
+
+
+function startGame() {
+    let activeCircle = document.querySelector(".circle.active");
+    if (!activeCircle) {
+        toggleCircle(`circle${currentId}`);
+    }
+    gameInterval = setInterval(() => {
+        toggleCircle(`circle${currentId}`);
+        let newId = randomId();
+        toggleCircle(`circle${newId}`);
+        currentId = newId;
+    }, 1000);
+}
+
+// startGame();
+
+startGameBtn.addEventListener("click", () => {
+    buttonsArea.style.display = "none";
+    gameArea.style.display = "flex";
+    startGame();
+})
