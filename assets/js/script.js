5628295da00a47d392eb5cca3931d0c3e2339ffe
Joel Leon Gfeller <joelleon.gfeller@icloud.com>
2023-07-31 09:23:16 +0100

    add event listeners to wait and drive btns

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,43 @@
+// Get a reference to the button element by its ID
+const waitBtn = document.getElementById("btn1");
+const driveBtn = document.getElementById("btn2");
+let gameInterval;
+let currentId = randomId();
+
+const correctScore = document.getElementById("correct-int");
+const incorrectScore = document.getElementById("incorrect-int");
+
 const gameArea = document.querySelector(".game-area");
 const buttonsArea = document.querySelector(".main-buttons");
 
 const startGameBtn = document.getElementById("start-game");
 const rulesBtn = document.getElementById("read-rules");
+
+// Add the event listener to the button 
+waitBtn.addEventListener("click", function () {
+    // This function will be executed when the button is clicked
+    const activeCircle = document.querySelector(".circle.active");
+
+    if (activeCircle.id === 'circle1' || activeCircle.id === 'circle2') {
+        correctScore.innerText = parseInt(correctScore.innerText) + 1
+    } else {
+        incorrectScore.innerText = parseInt(incorrectScore.innerText) + 1
+    }
+});
+
+driveBtn.addEventListener("click", function () {
+    // This function will be executed when the button is clicked
+    const activeCircle = document.querySelector(".circle.active");
+
+    if (activeCircle.id === 'circle3') {
+        correctScore.innerText = parseInt(correctScore.innerText) + 1
+
+    } else {
+        incorrectScore.innerText = parseInt(incorrectScore.innerText) + 1
+
+    }
+});
+
 
 function randomId() {
     return Math.ceil(Math.random() * 3);
